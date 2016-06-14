9c266b57ca227565d180b2046374dc71823bb003
AshishTechIT <ashish.techit@gmail.com>
2016-06-14 09:25:41 +0530

    Commit Hello World

---
+++ HELLO_WORLD/HELLO_WORLD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HELLO_WORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            Console.ReadLine();
+        }
+    }
+}
